6e9a711911874b87ff78cda99cc1376dd5f92b28
sidkhx <ryan.byloos@student.umons.ac.be>
2019-02-25 10:37:44 +0100

    correction lv4 to lv3

--- core/src/be/ac/umons/slay/g03/Entity/AttackTower.java
+++ core/src/be/ac/umons/slay/g03/Entity/AttackTower.java
@@ -23,7 +23,7 @@ public class AttackTower extends DefenceTower {
     public void attack(Cell cell) {
         if (cell.getElementOn() instanceof Soldier) {
             if ((((Soldier) cell.getElementOn()).getLevel() < this.getLevel()
-                    || this.getLevel() == 4 && ((Soldier) cell.getElementOn()).getLevel() == 4)
+                    || this.getLevel() == 3 && ((Soldier) cell.getElementOn()).getLevel() == 3)
                     && !cell.getElementOn().getOwner().equals(this.getOwner())) {
                 cell.setElementOn(null);
             }
