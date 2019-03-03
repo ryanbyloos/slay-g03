3bcd37cdf33a9eef655585a4c6a7d7a1beb9d493
dan <danial.joukar@student.umons.ac.be>
2019-03-03 18:15:45 +0100

    implemented redo/undo, logic behind replay.

---
+++ core/src/be/ac/umons/slay/g03/GameHandler/ReplayParserException.java
@@ -0,0 +1,4 @@
+package be.ac.umons.slay.g03.GameHandler;
+
+public class ReplayParserException extends Exception {
+}
---
+++ core/test/GameStateTest.java
@@ -0,0 +1,45 @@
+import be.ac.umons.slay.g03.Core.Cell;
+import be.ac.umons.slay.g03.Core.Map;
+import be.ac.umons.slay.g03.Core.Player;
+import be.ac.umons.slay.g03.Core.Territory;
+import be.ac.umons.slay.g03.GameHandler.GameState;
+import be.ac.umons.slay.g03.GameHandler.Loader;
+import be.ac.umons.slay.g03.GameHandler.ReplayParserException;
+import be.ac.umons.slay.g03.GameHandler.WrongFormatException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GameStateTest extends GameStageTest {
+    @Test
+    public void undoTest(){
+        Map map = new Map(new ArrayList<Cell>(), new Player("Danial", 1, 0, false, 0, new ArrayList<Territory>()),
+                new Player("Alex", 2, 0, false, 0, new ArrayList<Territory>()));
+        Loader loader = new Loader("test1.tmx", "test.xml", "wow");
+        try {
+            loader.loadFromTmxFile(map, false);
+            loader.loadFromXmlFile(map, false);
+            GameState gameState = new GameState(map, loader, 0, null);
+            gameState.saveReplay();
+            gameState.storeTurn();
+            gameState.storeMove(map.player1);
+            map.player1.setMoveNumber(1);
+            map.player1.setMaxMoveNumber(1);
+            map.findCell(1, 1).setElementOn(null);
+            gameState.storeMove(map.player1);
+            gameState.undo(map.player1);
+            Assert.assertNotEquals(null, gameState.getMap().findCell(1, 1).getElementOn());
+        } catch (ReplayParserException e) {
+            e.printStackTrace();
+        } catch (WrongFormatException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
