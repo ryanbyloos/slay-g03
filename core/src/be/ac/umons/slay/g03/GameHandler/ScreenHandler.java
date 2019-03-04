15d0908829937f9d1faec834abbd1c8ecf54da71
sidkhx <ryan.byloos@student.umons.ac.be>
2019-03-04 17:06:54 +0100

    new menu theme

--- core/src/be/ac/umons/slay/g03/GameHandler/ScreenHandler.java
+++ core/src/be/ac/umons/slay/g03/GameHandler/ScreenHandler.java
@@ -8,17 +8,17 @@ import be.ac.umons.slay.g03.Main;
 import com.badlogic.gdx.Screen;
 
 public class ScreenHandler {
+    public static int WIDTH;
+    public static int HEIGHT;
+    public static int BUTTON_WIDTH;
+    public static int BUTTON_HEIGHT;
+
     public static Menu menu = new Menu();
     public static Main game = new Main();
 
     public static Settings settings;
     public static HallOfFame hof;
     public static ReplayScreen replay;
-
-    public static int WIDTH;
-    public static int HEIGHT;
-    public static int BUTTON_WIDTH;
-    public static int BUTTON_HEIGHT;
 
     public static void setScreen(Screen screen) {
         game.setScreen(screen);
