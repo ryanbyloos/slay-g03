d5363542a8ee9d3b6bfd43c6740f6d71e7f3c05d
sidkhx <ryan.byloos@student.umons.ac.be>
2019-03-04 12:47:03 +0100

    refactoring entitytest and merging with soldiertest

--- core/test/EntityTest.java
+++ core/test/EntityTest.java
@@ -49,5 +49,51 @@ public class EntityTest {
         Assert.assertEquals(boat.getDefense(), 3);
     }
 
+    @Test
+    public void attackLowerSoldier() {
+        Soldier soldierLow = new Soldier(0, 0, null, 1, false);
+        Soldier soldierHigh = new Soldier(0, 0, null, 2, false);
+        Cell cell1 = new Cell(0, 0, false, false, null, soldierLow);
+        Cell cell2 = new Cell(0, 0, false, false, null, soldierHigh);
+        soldierHigh.attack(cell2, cell1);
+        Assert.assertSame(soldierHigh, cell2.getElementOn());
+        Assert.assertNotSame(soldierLow, cell1.getElementOn());
+
+    }
+
+    @Test
+    public void attackHigherSoldier() {
+        Soldier soldierLow = new Soldier(0, 0, null, 1, false);
+        Soldier soldierHigh = new Soldier(0, 0, null, 2, false);
+        Cell cell1 = new Cell(0, 0, false, false, null, soldierLow);
+        Cell cell2 = new Cell(0, 0, false, false, null, soldierHigh);
+        soldierLow.attack(cell1, cell2);
+        Assert.assertSame(soldierLow, cell1.getElementOn());
+        Assert.assertSame(soldierHigh, cell2.getElementOn());
+
+    }
+
+    @Test
+    public void attackEqualSoldier() {
+        Soldier soldier1 = new Soldier(0, 0, null, 1, false);
+        Soldier soldier2 = new Soldier(0, 0, null, 1, false);
+        Cell cell1 = new Cell(0, 0, false, false, null, soldier1);
+        Cell cell2 = new Cell(0, 0, false, false, null, soldier2);
+        soldier1.attack(cell1, cell2);
+        Assert.assertSame(soldier1, cell1.getElementOn());
+        Assert.assertSame(soldier2, cell2.getElementOn());
+    }
+
+    @Test
+    public void attackEqualHighSoldier() {
+        Soldier soldier1 = new Soldier(0, 0, null, 3, false);
+        Soldier soldier2 = new Soldier(0, 0, null, 3, false);
+        Cell cell1 = new Cell(0, 0, false, false, null, soldier1);
+        Cell cell2 = new Cell(0, 0, false, false, null, soldier2);
+        soldier1.attack(cell1, cell2);
+        Assert.assertNotEquals(soldier1, cell1.getElementOn());
+        Assert.assertNotEquals(soldier2, cell2.getElementOn());
+    }
+
 
 }
