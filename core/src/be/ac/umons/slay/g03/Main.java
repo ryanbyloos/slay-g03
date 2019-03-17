f91ecf5e5b6b64e5ecbcf4af80907d45b2f1b71a
sidkha <sidkha@disroot.org>
2019-03-17 11:35:01 +0100

    correction

--- core/src/be/ac/umons/slay/g03/Main.java
+++ core/src/be/ac/umons/slay/g03/Main.java
@@ -15,8 +15,8 @@ public class Main extends Game {
         prefs = Gdx.app.getPreferences("settings");
         if (!prefs.getBoolean("initialized")) {
             prefs.putBoolean("initialized", true);
-            prefs.putInteger("width", 640);
-            prefs.putInteger("height", 640);
+            prefs.putInteger("width", 800);
+            prefs.putInteger("height", 600);
             prefs.flush();
         }
         ScreenHandler.WIDTH = prefs.getInteger("width");
@@ -24,7 +24,6 @@ public class Main extends Game {
         ScreenHandler.BUTTON_WIDTH = ScreenHandler.WIDTH / 4;
         ScreenHandler.BUTTON_HEIGHT = ScreenHandler.HEIGHT / 12;
 
-//        skin = new Skin(Gdx.files.internal("assets/flat-earth/flat-earth-ui.json"));
         skin = new Skin(Gdx.files.internal("assets/pixel-theme/skin/skin.json"));
         skin.addRegions(skin.getAtlas());
         Gdx.graphics.setWindowedMode(ScreenHandler.WIDTH, ScreenHandler.HEIGHT);
