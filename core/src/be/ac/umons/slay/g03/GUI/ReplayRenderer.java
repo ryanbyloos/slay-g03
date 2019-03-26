d0745954c5342a5ad2cf618d347c6bcbbfb4a3e4
sidkha <ryan.byloos@student.umons.ac.be>
2019-03-26 20:51:20 +0100

    better replay

--- core/src/be/ac/umons/slay/g03/GUI/ReplayRenderer.java
+++ core/src/be/ac/umons/slay/g03/GUI/ReplayRenderer.java
@@ -7,6 +7,7 @@ import be.ac.umons.slay.g03.GameHandler.WrongFormatException;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -46,21 +47,23 @@ public class ReplayRenderer extends MapRenderer implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 replay.setAutoDisplay(true);
-                System.out.println("GNEH");
             }
         });
         stage.addActor(button);
         Gdx.input.setInputProcessor(stage);
         elapsed = 0f;
+        camera = new OrthographicCamera();
+        map = new Map(replayMap.get(replay.getTurnNumber()).get(replay.getMoveNumber()), replay.getP1(), replay.getP2());
+        setViewport(camera, map);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
         elapsed += delta;
         this.batch.begin();
-//        if (map==null)
         map = new Map(replayMap.get(replay.getTurnNumber()).get(replay.getMoveNumber()), replay.getP1(), replay.getP2());
         if (elapsed >= 1 && replay.isAutoDisplay()) {
             if (replay.next()) {
@@ -71,16 +74,32 @@ public class ReplayRenderer extends MapRenderer implements Screen {
                 if (!replay.nextTurn())
                     replay.stopAutoDisplay();
             }
-            System.out.println(replay.getMoveNumber() + " " + replay.getTurnNumber());
         }
-//        replay.stopAutoDisplay();
         draw(map);
         this.batch.end();
         stage.draw();
+        batch.setProjectionMatrix(camera.combined);
     }
 
     @Override
     public void hide() {
 
+    }
+
+    public int[] getSize() {
+        Cell cell = replayMap.get(0).get(0).get(replayMap.get(0).get(0).size() - 1);
+        return new int[]{cell.getX(), cell.getY()};
+    }
+
+    @Override
+    void setViewport(OrthographicCamera camera, Map map) {
+        int[] coord = getSize();
+        int mapH = coord[1];
+        int w = (coord[0] * 32) + 48;
+        int h = mapH * 24 + 8 + (32 * (mapH % 2));
+
+        System.out.println(w + " " + h);
+
+        camera.setToOrtho(false, w, h);
     }
 }
