6ff21cada733728f2ee5660be7927cefce2cd6b6
sidkha <ryan.byloos@student.umons.ac.be>
2019-03-27 22:58:02 +0100

    implemented slider in replay

--- core/src/be/ac/umons/slay/g03/GUI/ReplayRenderer.java
+++ core/src/be/ac/umons/slay/g03/GUI/ReplayRenderer.java
@@ -9,12 +9,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import java.util.ArrayList;
@@ -28,7 +30,7 @@ public class ReplayRenderer extends MapRenderer implements Screen {
 
     private Stage stage = new Stage();
     private Table table;
-    private TextButton playButton, returnButton, speedButton;
+    private TextButton playButton, returnButton;
     private Slider slider;
 
     public ReplayRenderer(Replay replay, String replayFileName) {
@@ -43,7 +45,6 @@ public class ReplayRenderer extends MapRenderer implements Screen {
         returnButton = new TextButton("RETURN", Slay.game.skin);
         table = new Table(Slay.game.skin).top().left().pad(10);
         table.setFillParent(true);
-//        slider = new Slider(0, )
 
         replay.setReplayFileName(this.replayFileName);
         try {
@@ -51,7 +52,7 @@ public class ReplayRenderer extends MapRenderer implements Screen {
         } catch (WrongFormatException e) {
             e.printStackTrace();
         }
-
+        slider = new Slider(0, replay.getTotalMove() - 1, 1, false, Slay.game.skin);
         replayMap = replay.getReplay();
         this.batch = new SpriteBatch();
 
@@ -70,13 +71,37 @@ public class ReplayRenderer extends MapRenderer implements Screen {
             }
         });
 
+        slider.addListener(new ChangeListener() {
+            int tmp = 0;
+
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                map = new Map(replayMap.get(replay.getTurn()).get(replay.getMove()), replay.getP1(), replay.getP2());
+                int v = (int) slider.getValue() - tmp;
+                if (v > 0) {
+                    for (int i = 0; i < v; i++) {
+                        replay.next();
+                    }
+                }
+                if (v < 0) {
+                    for (int i = 0; i > v; i--) {
+                        replay.previous();
+                    }
+                }
+                tmp = (int) slider.getValue();
+            }
+        });
+
+
+
         table.add(returnButton).pad(5).padRight(returnButton.getWidth());
         table.add(playButton).pad(5);
+        table.add(slider).padLeft(Slay.w / 8).width(Slay.w / 2);
         stage.addActor(table);
         Gdx.input.setInputProcessor(stage);
 
         camera = new OrthographicCamera();
-        map = new Map(replayMap.get(replay.getTurnNumber()).get(replay.getMoveNumber()), replay.getP1(), replay.getP2());
+        map = new Map(replayMap.get(replay.getTurn()).get(replay.getMove()), replay.getP1(), replay.getP2());
         setViewport(camera, map);
         camera.zoom = 1.2f;
         elapsed = 0f;
@@ -89,11 +114,11 @@ public class ReplayRenderer extends MapRenderer implements Screen {
         camera.update();
         elapsed += delta;
         this.batch.begin();
-        map = new Map(replayMap.get(replay.getTurnNumber()).get(replay.getMoveNumber()), replay.getP1(), replay.getP2());
-        if (elapsed >= .05 && replay.isAutoDisplay()) {
-            replay.next();
-                map = new Map(replayMap.get(replay.getTurnNumber()).get(replay.getMoveNumber()), replay.getP1(), replay.getP2());
+        if (replay.isAutoDisplay()) {
+            if (elapsed >= .05) {
+                slider.setValue(slider.getValue() + 1f);
                 elapsed = 0;
+            }
         }
         draw(map);
         this.batch.end();
@@ -104,6 +129,7 @@ public class ReplayRenderer extends MapRenderer implements Screen {
             playButton.setLabel(new Label("STOP", Slay.game.skin));
         } else
             playButton.setLabel(new Label("PLAY", Slay.game.skin));
+
     }
 
     @Override
